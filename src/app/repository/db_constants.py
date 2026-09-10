cf960dd4f5977f69973d1c5bce6b174c58f24963
dantedavidsondaniele <dantedavidsondaniele@gmail.com>
2026-09-10 10:33:33 +1000

    post stats endpoint

--- src/app/repository/db_constants.py
+++ src/app/repository/db_constants.py
@@ -1,2 +1,3 @@
 ITEMS_DB_NAME: str = "items"
 TRADE_DB_NAME: str = "trade"
+STAT_DB_NAME: str = "stats"
--- src/app/router/item.py
+++ src/app/router/item.py
@@ -32,9 +32,14 @@ class ItemRouter(AppRouter):
         @self._router.get("/stats")
         async def get_item_categories(service:ItemService = Depends(item_service)):
             return await service.get_stats()
+    def _post_stats(self):
+        @self._router.post("/stats")
+        async def post_stats(service:ItemService = Depends(item_service)):
+            return await service.post_stats()
         
     def _init_routes(self):
         self._get_all()
         self._get_ids()
         self._get_item_categories()
         self._get_stats()
+        self._post_stats()
